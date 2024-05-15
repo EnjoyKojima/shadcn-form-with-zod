d9aa39ad5d4ccb14de01d51383a9128776404bff
EnjoyKojima <majiko2627kojima@gmail.com>
2024-05-15 21:13:52 +0900

    typescriptをドキュメント通りに追加したらランタイムのエラー解消

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -2,9 +2,8 @@
 import type { HTMLAttributes } from 'vue'
 import { Primitive, type PrimitiveProps } from 'radix-vue'
 import { type ButtonVariants, buttonVariants } from '.'
-import { cn } from '@/lib/utils'
 
-interface Props extends /* @vue-ignore */ PrimitiveProps {
+interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   class?: HTMLAttributes['class']
